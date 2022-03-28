37fc7c5d3819db9f5b4ae0a0b64545959f4ecdbf
di-feb <sdi1900045@di.uoa.gr>
2022-03-28 22:37:07 +0300

    Update the proj

--- Project_1/Calculator.java
+++ Project_1/Calculator.java
@@ -33,20 +33,84 @@ class Evaluator {
         lookahead = in.read();
     }
 
-    private void Exp() {
-        return;
+    private void consume(int symbol) throws IOException, ParseError {
+        if (lookahead == symbol)
+            lookahead = in.read();
+        else
+            throw new ParseError();
     }
 
-    private void Term(){
-        return;
+    private boolean isDigit(int c) { return '0' <= c && c <= '9'; }
+    private boolean isLeftParenthesis(int c) { return '(' == c; }
+    private boolean isRightParenthesis(int c) { return ')' == c; }
+    private boolean isANDoperator(int c) { return '&' == c; }
+    private boolean isXORoperator(int c) { return '^' == c; }
+    private boolean isEOF(int c) { return ( c == '\n' || c == -1 ); }
+    private int evalDigit(int c){ return c - '0'; }
+
+    private int Exp() throws IOException, ParseError{
+        if(!isDigit(lookahead) && !isLeftParenthesis(lookahead)) throw new ParseError();
+        return Rest(Term());
     }
-    private void Rest(){
-        return;
+
+    private int Term() throws IOException, ParseError {
+        if(!isDigit(lookahead) && !isLeftParenthesis(lookahead)) throw new ParseError();
+        return Rest2(Factor());
     }
-    private void Rest2(){
-        return;
+
+    private int Rest(int num) throws IOException, ParseError{
+        if(isDigit(lookahead) || isANDoperator(lookahead) || isLeftParenthesis(lookahead)) throw new ParseError();
+        if(isEOF(lookahead) || isRightParenthesis(lookahead)){
+            return 0;
+        }
+        else{
+            consume(lookahead);
+            int num2 = Rest(Term());
+            return (num ^ num2);  
+        }
     }
-    private void Factor(){
-        return;
+    private int Rest2(int num) throws IOException, ParseError{
+        if(isDigit(lookahead) || isLeftParenthesis(lookahead)) throw new ParseError();
+        if(isXORoperator(lookahead) || isEOF(lookahead) || isRightParenthesis(lookahead)){
+            return 0;
+        }
+        else {
+            consume(lookahead);
+            int num2 = Rest2(Factor());
+            return (num & num2);
+        }
+    }
+    private int Factor() throws IOException, ParseError{
+        if(!isDigit(lookahead) && !isLeftParenthesis(lookahead)) throw new ParseError();
+        if(isDigit(lookahead)){
+            int cond = evalDigit(lookahead);    
+            consume(lookahead);
+            return cond;
+        }
+        else{
+            consume(lookahead);
+            int cond = Exp();
+            consume(')');
+            return cond;
+        }
+    }
+    
+    public int eval() throws IOException, ParseError {
+        int value = Tern();
+    
+        if (lookahead != -1 && lookahead != '\n')
+            throw new ParseError();
+    
+        return value;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        try {
+            System.out.println((new Evaluator(System.in)).eval());
+        } catch (IOException | ParseError e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
