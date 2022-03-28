8d50c290794b45183eced052a1437d4ebb39eabc
di-feb <sdi1900045@di.uoa.gr>
2022-03-28 22:36:45 +0300

    Put colour at the output comment

--- Project_1/ParseError.java
+++ Project_1/ParseError.java
@@ -1,5 +1,8 @@
 public class ParseError extends Exception {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public String getMessage() {
-	    return "parse error";
+	    return (ANSI_RED + "Parse Error!" + ANSI_RESET);
     }
 }
