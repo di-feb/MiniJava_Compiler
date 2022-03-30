753ac075c4b642133686cd8628c076c3e8f5d981
di-feb <sdi1900045@di.uoa.gr>
2022-03-30 16:14:39 +0300

    Reorganize the files

--- Project_1/ParseError.java
+++
@@ -1,8 +0,0 @@
-public class ParseError extends Exception {
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_RESET = "\u001B[0m";
-
-    public String getMessage() {
-	    return (ANSI_RED + "Parse Error!" + ANSI_RESET);
-    }
-}
