82947e47f92e834b7bd9ff4fa022316b07197950
di-feb <sdi1900045@di.uoa.gr>
2022-04-16 18:15:04 +0300

    Remove final

--- Project_2/source/MethodInfo.java
+++ Project_2/source/MethodInfo.java
@@ -4,14 +4,13 @@ import visitor.GJDepthFirst;
 import syntaxtree.*;
 
 public class MethodInfo {
-    final private String returnType;            // Return type of the method
-    final private Integer offset;               // Offset of the method
-    final private Vector < String > arguments;  // Args of the method
-    final private Vector < String > vars;       // Any other var that has been declared inside the scope of the method
+    private String returnType;            // Return type of the method
+    private Integer offset;               // Offset of the method
+    private Vector < String > arguments;  // Args of the method
+    private Vector < String > vars;       // Any other var that has been declared inside the scope of the method
 
     // Constructor
-    MethodInfo(String name){
-        this.name = name;
+    MethodInfo(){
         this.arguments = new Vector<String>();
         this.vars = new Vector<String>();
     }
