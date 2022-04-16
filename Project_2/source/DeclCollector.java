23da0cfcddd7b2fa71dc2e6db3546652692e7b4a
di-feb <sdi1900045@di.uoa.gr>
2022-04-16 18:14:35 +0300

    Fix MainClass visit

--- Project_2/source/DeclCollector.java
+++ Project_2/source/DeclCollector.java
@@ -5,23 +5,23 @@ import java.util.LinkedHashMap;
 import visitor.GJDepthFirst;
 import syntaxtree.*;
 
-public class DeclCollector extends GJDepthFirst< String, String >{
+public class DeclCollector extends GJDepthFirst< String, Data >{
     private Map <String, Data> symbol_table;
 
     public DeclCollector(){
-        this.symbol_table = new Map < String, Data >();
+        this.symbol_table = new LinkedHashMap < String, Data >();
     }
+
     /** Goal
     * f0 -> MainClass()
     * f1 -> ( TypeDeclaration() )*
     */
-    @Override
-    public String visit(Goal n, Integer argu) throws Exception {
+    public String visit(Goal n, Data data) throws Exception {
         // Accept at main class
         n.f0.accept(this, null);
         // When a production has a * it means that this production can appear
         // zero or more times. So for the  ( TypeDeclaration() )* f.e. we need to
-        // iterate all the declarations. 
+        // iterate all the classes declarations. 
         for( int i = 0; i < n.f1.size(); i++ )
             n.f1.elementAt(i).accept(this, null);
 
@@ -41,9 +41,26 @@ public class DeclCollector extends GJDepthFirst< String, String >{
         }
     }
     */
-    public String visit(MainClass n, A argu) throws Exception {
+    public String visit(MainClass n, Data data) throws Exception {
+        // Keep the name of the "main" class
+        String name = n.f1.accept(this, null);
 
+        // Allocate memory for a Data Object to fill it up with the info
+        // provided by VarDecls and Statements.
+        Data mains_data = new Data();
 
+        // Pass mains_data down to parse tree to collect the info
+        for( int i = 0; i < n.f14.size(); i++ )
+            n.f14.elementAt(i).accept(this, mains_data);
+
+        for( int i = 0; i < n.f15.size(); i++ )
+            n.f15.elementAt(i).accept(this, mains_data);
+
+        // After the infos for the "main" classes gathered push the data into the Map
+        symbol_table.put(name, data);
         return null;
     }
+
+
+    
 }
