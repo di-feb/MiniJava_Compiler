b1d5263d4770bfad00f805a8716b5079babe1442
di-feb <sdi1900045@di.uoa.gr>
2022-04-18 14:09:55 +0300

    Change accessors

--- Project_2/source/Data.java
+++ Project_2/source/Data.java
@@ -12,19 +12,18 @@ public class Data {
     // Name of the ParentClass of this current class
     // if parent class does not exist string is Null
     final private String parentClassName;                 
-    private Map < String, MethodInfo > method_info; // Key:Method_Name, Value:Class that contains info about that method
-    private Map < String, VarInfo > var_info;       // Key:Var_Name, Value:Class that contains info about that var
+    private Map < String, MethodInfo > methods; // Key:Method_Name, Value:Class that contains info about that method
+    private Map < String, VarInfo > vars;       // Key:Var_Name, Value:Class that contains info about that var
 
     // Constructor
     Data(String parentName){
         parentClassName = parentName;
-        method_info = new LinkedHashMap<String, MethodInfo>();
-        var_info = new LinkedHashMap<String, VarInfo>();
+        methods = new LinkedHashMap<String, MethodInfo>();
+        vars = new LinkedHashMap<String, VarInfo>();
     }
 
     // Accessors
     public String getName(){ return parentClassName; }
-    public Map getMethod_Info() { return method_info; }
-    public Map getVar_Info() { return var_info; }
-
+    public Map getMethods() { return methods; }
+    public Map getVars() { return vars; }
 }
