97dc7ed5717d366704a860b017077763c7942a1e
di-feb <sdi1900045@di.uoa.gr>
2022-04-26 14:32:57 +0300

    Change imports

--- Project_2/source/Data.java
+++ Project_2/source/Data.java
@@ -1,8 +1,7 @@
 import java.util.Map;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.HashMap; 
+import java.util.ArrayList;
 import visitor.GJDepthFirst;
 import syntaxtree.*;
 
